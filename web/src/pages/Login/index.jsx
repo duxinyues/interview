c5c611375644b8336433219d2526439babcf53c5
duxinyues <weiyy26445@yunrong.cn>
2023-08-15 09:11:54 +0800

    feat:封装表单

--- web/src/pages/Login/index.jsx
+++ web/src/pages/Login/index.jsx
@@ -2,15 +2,16 @@
  * @Author: duxinyues weiyy26445@yunrong.cn
  * @Date: 2023-08-15 00:43:20
  * @LastEditors: duxinyues weiyy26445@yunrong.cn
- * @LastEditTime: 2023-08-15 03:02:02
+ * @LastEditTime: 2023-08-15 09:07:41
  * @FilePath: /web/src/pages/Login/index.jsx
  * @Description: 
  * Copyright (c) 2023 by ${duxinyues} email: ${weiyy26445@yunrong.cn}, All Rights Reserved.
  */
+
 /**
  * 登录页
  */
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Form from "@/components/Form/Form";
 import Input from "@/components/Form/Input";
 import FormItem from "@/components/Form/FormItem";
@@ -18,24 +19,29 @@ import "./index.scss"
 
 function Login() {
   const form = useRef(null);
+  const [tips, setTips] = useState(null)
   /* 表单提交 */
   const submit = () => {
     form.current.submitForm((formValue) => {
       console.log(formValue)
-      if(!formValue.userName){
-       return alert("请输入用户名")
+      if (!formValue.userName) {
+        return setTips("请输入用户名");
       }
 
-      if(!formValue.password){
-        return alert("请输入密码")
-       }
+      if (!formValue.password) {
+        return setTips("请输入密码")
+      }
+
+      setTips(null)
     })
   }
   /* 重置表单 */
   const reset = () => {
-    form.current.resetForm()
+    form.current.resetForm();
+    setTips(null)
   }
   return <div className="login-content">
+    <h3>欢迎登录</h3>
     <Form ref={form}>
       <FormItem name="userName" label="用户名">
         <Input />
@@ -48,7 +54,7 @@ function Login() {
       <button className="searchbtn" onClick={submit} >提交</button>
       <button className="concellbtn" onClick={reset} >重置</button>
     </div>
-
+    <p className="tips">{tips}</p>
   </div>
 }
 
